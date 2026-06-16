20cc6888559e7e987e123896973a08de5c4fc37e
Solange <solangegrima8@gmail.com>
2026-06-15 23:53:54 -0300

    Se agregan las entidades del dominio

---
+++ Dsw2026Ej15.Domain/Entities/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    public class Doctor : BaseEntity
+    {
+        public string Name { get; init; }
+
+        public string LicenseNumber { get; init; }
+
+        public bool IsActive { get; init; }
+
+        public Speciality Speciality { get; init; }
+
+        public Doctor(
+            string name,
+            string licenseNumber,
+            bool isActive,
+            Speciality speciality,
+            Guid? id = null) : base(id)
+        {
+            Name = name;
+            LicenseNumber = licenseNumber;
+            IsActive = isActive;
+            Speciality = speciality;
+        }
+    }
+}
---
+++ Dsw2026Ej15.Domain/Entities/Speciality.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej15.Domain.Entities
+{
+    public class Speciality
+    {
+        public string Name { get; init; }
+
+        public string Description { get; init; }
+
+        public Speciality(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
